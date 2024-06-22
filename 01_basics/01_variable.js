e48d5238b192511de4a3f16dcefb06d058757ed6
sanjeev0308 <sanjeev99019@gmail.com>
2024-06-22 09:45:34 +0530

    moveing in folder

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId =112211
+let accountEmail ="sanjeev00@google.com"
+ var accountPassword ="12345"
+ accountCity ="narot"
+ let accountState;
+ 
+ accountEmail = "sk999google.com"
+ accountPassword = "54321"
+ accountCity = "pathankot"
+
+ console.log(accountId);
+ 
+ console.table([ accountEmail, accountId, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+let name = "sanju"
+let age = 21
+let isLoggedIn = false
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
