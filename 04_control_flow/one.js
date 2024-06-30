c4f3e5f20d86e3b692c7ec6ab758626085635af7
sanjeev0308 <sanjeev99019@gmail.com>
2024-06-30 16:13:24 +0530

    basic if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if
+const isUserloggedIn = true    
+const temperature = 41
+
+// if ( temperature === 41 ){
+    // console.log("less than 50")
+
+// } else {
+    // console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+ 
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+ 
+// if (balance > 500) 
+//     console.log("test1");
+//     console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than ");
+ 
+// } else if (balance < 750){
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 900 ");
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userloggedIn = true
+const debitCard = true
+const loggedInGoogle = false
+const loggedInEmail = true
+
+if (userloggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if (loggedInGoogle || loggedInEmail ) {
+    console.log("User logged in");
+}
