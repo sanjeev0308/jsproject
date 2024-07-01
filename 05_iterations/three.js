9e5b5cc0e30dad048687071e2add1f6c454d173b
sanjeev0308 <sanjeev99019@gmail.com>
2024-07-01 14:15:08 +0530

    maps

---
+++ 05_iterations/three.js
@@ -0,0 +1,38 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr){
+    // console.log(num);
+
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// maps 
+
+const map = new Map()
+map.set("IN", "India")
+map.set("US", "United states")
+map.set("FR", "France")
+
+// console.log(map);
+
+for (const[key, value] of map){
+    console.log(key, ':-', value);
+}
+ const myObject ={
+    'game1': 'NFC',
+    'game2': 'spiderman'
+
+ }
+ // for (const [key, value] of myObject) {
+//    console.log(key, ':-' value);
+
+// }
